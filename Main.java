da0f9bbc22fe090f75c4f2d0f2e8453bbc87d7b5
Hoseok Yoon <totkfa727@gmail.com>
2026-03-03 13:20:53 +0900

    20260303 Java
    문풀1문제 정답

--- Main.java
+++ Main.java
@@ -1,18 +1,54 @@
-// 선생님이 내주신 수업시간에 푸는 문제들 VSCODE 이용 풀이 
-public class Main {
-    public static void main(String[] args) {
-        Car a = new Car();
-        a.name = "마티즈";
-        a.length = 125.5;
-        a.CarNo = 1;
-        a.colar = "황금색";
-        System.out.println(a);
+import java.util.ArrayList;
+
+interface Actions {
+    public void attack();
+
+    public void move();
+}
+
+class Ionia implements Actions {
+    String name;
+    int ad;
+    int ap;
+    int hp;
+
+    Ionia(String name, int ad){
+        this.ad=ad;
+        this.name= name;
+        
+    }
+    
+    public void attack() {
+        System.out.println(name + "이(가) 공격합니다.");
+    }
+
+    @Override
+    public void move() {
+        System.out.println(name + "이(가) 이동합니다.");
+}
+}
+class Yasuo extends Ionia{
+    Yasuo(String name,int ad){
+        super(name, ad);
+    }
+    @Override 
+
+    public void attack(){
+        System.out.println("야스오가 회오리를 날려 공격합니다");
+            this.ad +=1;
+        
+    }
+    @Override 
+
+    public void move(){
+        System.out.println((this.name)+" 이 이동합니다");
     }
 }
 
-class Car {
-    String name;
-    double length;
-    int CarNo;
-    String colar;
+
+class Main {
+    public static void main(String[] args) {
+        Ionia yasuo = new Yasuo("야스오", 150);
+        yasuo.attack();
+    }
 }
